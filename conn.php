78648880d0a9d6e2539faf7df9c74fc160bc32de
sushil <65329897+sushilthelearner@users.noreply.github.com>
2021-07-02 23:08:07 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,8 @@
 <?php
   $con=mysqli_connect('localhost','root');
   mysqli_select_db($con,'infostride_demo');
+  
+// uncomment Following code check for databse connection established or not
     //  if($con){
     //    echo 'Connected';
     //  }
